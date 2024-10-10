964b4e32913cf48d8c2a258bb732f90802958695
Daniel Jankowski <djank0113@gmail.com>
2024-10-10 18:20:26 -0400

    add fonts to bootstrap and update other depended

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.jsx';
+import './styles/css/reset.css';
 import './styles/scss/main.scss';
 import * as bootstrap from 'bootstrap';
 
