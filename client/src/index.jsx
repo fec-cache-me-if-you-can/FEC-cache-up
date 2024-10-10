30b08ec5b4d3dfe8a684c2d3219e8510a350d0aa
Daniel Jankowski <djank0113@gmail.com>
2024-10-10 14:17:57 -0400

    Various CSS, SCSS related things, font imports, add html template, import bootstrap

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.jsx';
+import './styles/scss/main.scss';
+import * as bootstrap from 'bootstrap';
 
 ReactDOM.render(<App />, document.getElementById('root'));
