d9fe9ad1210e4d4ed5c93ab1b18bc5c120ad2ec6
evelyons <jane90028@gmail.com>
2024-10-10 10:29:08 -0500

    created a drop down selector component

--- client/src/components/DropdownSelector.jsx
+++ client/src/components/DropdownSelector.jsx
@@ -1,9 +1,27 @@
+import React from 'react';
 import { useState } from 'react';
 
-export default function DropdownSelector () {
+export default function DropdownSelector({ options, defaultText, isDisabled, onChange }) {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleSelectChange = (event) => {
+    const value = event.target.value;
+    setSelectedOption(value);
+    onChange(value);
+  };
 
   return (
-    <>
-    </>
+    <select value={selectedOption} onChange={handleSelectChange} disabled={isDisabled || options.length === 0}>
+      <option value="">{defaultText || "Select Size"}</option>
+      {options.length === 0 ? (
+        <option value="">OUT OF STOCK</option>
+      ) : (
+        options.map((option) => (
+          <option key={option.size} value={option.size}>
+            {option.size}
+          </option>
+        ))
+      )}
+    </select>
   );
-};
+}
