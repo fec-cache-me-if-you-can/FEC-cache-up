503ced4b07be30801544b8124b2b412016ba00e1
evelyons <jane90028@gmail.com>
2024-10-10 14:20:51 -0500

    added button component and made progress on the cart

--- client/src/components/PrimaryButton.jsx
+++ client/src/components/PrimaryButton.jsx
@@ -1,9 +1,14 @@
+import React from 'react';
 import { useState } from 'react';
 
-export default function PrimaryButton () {
-
+export default function PrimaryButton ({ label, onClick, isDisabled = false, className = ''}) {
   return (
-    <>
-    </>
+    <button
+      className={`button-component ${className}`}
+      onClick={onClick}
+      disabled={isDisabled}
+    >
+      {label}
+    </button>
   );
-};
+}
