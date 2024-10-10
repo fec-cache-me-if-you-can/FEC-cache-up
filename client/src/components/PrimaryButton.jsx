c2dd8ee1735b00330c217dd8983e1bd1f536c1c7
Daniel Jankowski <djank0113@gmail.com>
2024-10-10 16:02:58 -0400

    Refactor PrimaryButton component to improve code structure and add prop type validation

--- client/src/components/PrimaryButton.jsx
+++ client/src/components/PrimaryButton.jsx
@@ -1,9 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function PrimaryButton({ label, onClick, isDisabled = false}) {
+  const buttonStyle = "btn btn-primary";
+  const labelStyle = "";
+  return (
+    <button 
+      onClick={onClick} 
+      className={buttonStyle}
+    >
+      <span className={labelStyle}>{label}</span>
+    </button>
+  );
+}
+
+// Prop type validation
+PrimaryButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+// Default props
+PrimaryButton.defaultProps = {
+  onClick: null,
+};
+
+import React from 'react';
 import { useState } from 'react';
 
-export default function PrimaryButton () {
-
+export default function PrimaryButton ({ label, onClick, isDisabled = false, className = ''}) {
   return (
-    <>
-    </>
+    <button
+
+    >
+      {label}
+    </button>
   );
-};
+}
