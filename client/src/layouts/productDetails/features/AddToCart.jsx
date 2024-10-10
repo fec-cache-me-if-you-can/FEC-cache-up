c54f7da938b4c626f518ced7255a5c8b033fd3a6
evelyons <jane90028@gmail.com>
2024-10-10 11:27:19 -0500

    wrote test for dropdown component and added enviromental stuff for jest testing

--- client/src/layouts/productDetails/features/AddToCart.jsx
+++ client/src/layouts/productDetails/features/AddToCart.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import DropdownSelector from '../../../components/DropdownSelector.jsx';
+// import axios from 'axios';
 
 export default function AddToCart () {
 
+  const [selectedSize, setSelectedSize] = useState('');
+  // const [selectedQuantity, setSelectedQuantity] = useState('');
+  // const [isDropdownDisabled, setIsDropdownDisabled] = useState(false);
+
+  const handleSizeChange = (size) => {
+    console.log("Selected size:", size);
+    setSelectedSize(size);
+  };
+
+  const sizeOptions = [
+    { size: "S", skuId: 1 },
+    { size: "M", skuId: 2 },
+    { size: "L", skuId: 3 },
+    { size: "XL", skuId: 4 },
+  ];
+
   return (
-    <div>AddToCart</div>
+    <div>
+    <DropdownSelector
+      options={sizeOptions}
+      defaultText="Select Size"
+      isDisabled={true}
+      onChange={handleSizeChange}
+    />
+    </div>
   );
 }
+
+
+
