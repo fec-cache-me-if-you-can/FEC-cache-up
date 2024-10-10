d0a8ffe7e36f32c6e135231fed46f944e3131e23
itsWalkerRichard <walker@teamrichard.com>
2024-10-09 20:28:39 -0600

    cleaned unnecessary comments

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,9 +1,6 @@
 const controllers = require('../controllers');
 const router = require('express').Router();
 
-// example
-// router.get('/messages', controller.messages.get);
-
 router.get('/', controllers.products.getAll);
 router.get('/information', controllers.products.getProductInformation);
 router.get('/styles', controllers.products.getProductStyles);
