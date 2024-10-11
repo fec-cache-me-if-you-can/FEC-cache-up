c194f6f6b6d204f19904067354fa91c09fc61018
Daniel Jankowski <djank0113@gmail.com>
2024-10-11 01:49:35 -0400

    Add SCSS files for functions, variables, mixins, buttons, and dropdown components

--- client/src/components/PrimaryButton.jsx
+++ client/src/components/PrimaryButton.jsx
@@ -1,14 +1,17 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function PrimaryButton({ label, onClick, isDisabled = false}) {
-  const buttonStyle = "btn btn-primary";
-  const labelStyle = "";
+export default function PrimaryButton({
+  label = 'unnamed button',
+  onClick = null,
+  isDisabled = false,
+  extraStyles = '',
+  plus = false,
+}) {
+  const buttonStyle = `btn btn-primary square btn-lg m-3 ${extraStyles}`;
+  const labelStyle = '';
   return (
-    <button 
-      onClick={onClick} 
-      className={buttonStyle}
-    >
+    <button onClick={onClick} className={buttonStyle}>
       <span className={labelStyle}>{label}</span>
     </button>
   );
@@ -16,24 +19,9 @@ export default function PrimaryButton({ label, onClick, isDisabled = false}) {
 
 // Prop type validation
 PrimaryButton.propTypes = {
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string,
   onClick: PropTypes.func,
+  isDisabled: PropTypes.bool,
+  extraStyles: PropTypes.string,
+  plus: PropTypes.bool,
 };
-
-// Default props
-PrimaryButton.defaultProps = {
-  onClick: null,
-};
-
-import React from 'react';
-import { useState } from 'react';
-
-export default function PrimaryButton ({ label, onClick, isDisabled = false, className = ''}) {
-  return (
-    <button
-
-    >
-      {label}
-    </button>
-  );
-}
