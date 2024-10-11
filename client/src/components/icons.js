798b793b7bf253a69dbc460d122e93a8adb315ea
Daniel Jankowski <djank0113@gmail.com>
2024-10-11 15:19:31 -0400

    remove package-lock.json from git tracking

---
+++ client/src/components/icons.js
@@ -0,0 +1,7 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { all } from '@awesome.me/kit-60350fbc81/icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(...all);
+
+export default FontAwesomeIcon;
