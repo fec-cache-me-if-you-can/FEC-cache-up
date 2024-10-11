5d161a6430768fd38b9bdd5b6928a21b29655f87
itsWalkerRichard <walker@teamrichard.com>
2024-10-10 20:04:05 -0600

    finished and tested cart API

--- server/controllers/cart.js
+++ server/controllers/cart.js
@@ -1,6 +1,19 @@
 const models = require('../models');
 
 module.exports = {
-  get: (req, res) => { },
-  post: (req, res) => { },
+  get: (req, res) => {
+    models.cart
+      .fetch()
+      .then((result) => res.send(result.data))
+      .catch(() => res.sendStatus(400));
+  },
+  post: (req, res) => {
+    if (!req.body.sku_id) {
+      return res.sendStatus(400);
+    }
+    models.cart
+      .create(req.body)
+      .then(() => res.sendStatus(201))
+      .catch(() => res.sendStatus(400));
+  },
 };
--- server/models/cart.js
+++ server/models/cart.js
@@ -2,6 +2,18 @@ const axios = require('axios');
 require('dotenv').config();
 
 module.exports = {
-  fetch: () => { },
-  create: () => { },
+  fetch: () => {
+    return axios.get(`${process.env.API_URL}cart`, {
+      headers: {
+        Authorization: process.env.GITHUB_TOKEN,
+      },
+    });
+  },
+  create: (body) => {
+    return axios.post(`${process.env.API_URL}cart`, body, {
+      headers: {
+        Authorization: process.env.GITHUB_TOKEN,
+      },
+    });
+  },
 };
