bdf43b2acaa23a52beb10024f201b7ad318f15fa
Daniel Jankowski <djank0113@gmail.com>
2024-10-14 17:11:11 -0400

    Add FacebookIcon, PinterestIcon, and XIcon components

---
+++ client/src/components/FacebookIcon.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+
+const FacebookIcon = ({ size = '' }) => {
+  return <FontAwesomeIcon icon={faFacebook} size={size} />;
+};
+
+FacebookIcon.propTypes = {
+  size: PropTypes.string.isRequired,
+};
+
+export default FacebookIcon;
