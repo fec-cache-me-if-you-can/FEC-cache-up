63937b9315757ee374b33fe94d012a6b64654a10
Daniel Jankowski <djank0113@gmail.com>
2024-10-15 01:59:00 -0400

    - Introduced `AbstractProductCard` for shared card logic. - Added `OutfitCard` and `DeleteButton` components. - Updated `OutfitList` to display a list of outfit cards using Swiper. - Modified `ProductDetails` to accept a renderIcon prop. - Simplified `RelatedProductCard` by using `AbstractProductCard`. - Refactored `favoriteToggle` to prevent default form submission. - Enhanced styles for favorite and delete buttons.

---
+++ client/src/layouts/relatedItemsAndOutfitCreation/features/DeleteButton.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Icon from '../../../components/icons.jsx';
+import PropTypes from 'prop-types';
+
+export default function DeleteButton({ productId, onDelete }) {
+  const handleDelete = (e) => {
+    e.preventDefault();
+    const button = e.currentTarget;
+    button.classList.add('clicked');
+    setTimeout(() => {
+      button.classList.remove('clicked');
+      onDelete(productId);
+    }, 300);
+  };
+
+  return (
+    <button className="transparent-button trash-button" onClick={handleDelete}>
+      <Icon icon={'fa-trash-alt fa-solid fa-lg'} />
+    </button>
+  );
+}
+
+DeleteButton.propTypes = {
+  productId: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
--- client/src/layouts/relatedItemsAndOutfitCreation/features/OutfitList.jsx
+++ client/src/layouts/relatedItemsAndOutfitCreation/features/OutfitList.jsx
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import OutfitCard from './OutfitCard.jsx';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/scss/navigation';
 
-export default function OutfitList() {
-  return <div>OutfitList</div>;
-}
+const OutfitList = () => {
+  const [outfitIds, setOutfitIds] = useState([]);
+
+  const mockOutfitIds = [
+    40345, 40346, 40351, 40350, 40349, 40349, 40351, 40352, 40344, 40346,
+  ];
+
+  useEffect(() => {
+    // TODO: fetch users outfitIds from the server
+    setOutfitIds(mockOutfitIds);
+  }, []);
+
+  // TODO: implement static "add" card
+  return (
+    <div className="container flex-grow-1">
+      <h5>Your Outfit</h5>
+      <Swiper
+        spaceBetween={10}
+        slidesPerView={3}
+        loop={true}
+        centeredSlides={true}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        {outfitIds.map((id) => {
+          return (
+            <div key={id} className="container bg-secondary">
+              <SwiperSlide>
+                <OutfitCard productId={id} />
+              </SwiperSlide>
+            </div>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default OutfitList;
