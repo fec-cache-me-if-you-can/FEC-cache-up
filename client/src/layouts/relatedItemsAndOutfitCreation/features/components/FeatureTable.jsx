134ebad1a36cd4e060915e4d4ceb2d08a85046a1
Daniel Jankowski <djank0113@gmail.com>
2024-10-17 18:07:14 -0400

    Factor out table for comparison modal

---
+++ client/src/layouts/relatedItemsAndOutfitCreation/features/components/FeatureTable.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  getFeatureValue,
+  createFeatureMap,
+  getAllFeatureNames,
+} from '../../utils';
+
+const FeatureTable = ({ relatedProduct, selectedProduct }) => {
+  const { name: relatedProductName, features: relatedFeatures } =
+    relatedProduct;
+  const { name: selectedProductName, features: selectedFeatures } =
+    selectedProduct;
+
+  if (!Array.isArray(relatedFeatures) || !Array.isArray(selectedFeatures)) {
+    return <div>Invalid product features</div>;
+  }
+
+  const featureMapForRelatedProduct = createFeatureMap(relatedFeatures);
+  const allFeatureNames = getAllFeatureNames(relatedFeatures, selectedFeatures);
+
+  const renderFeatureRows = () =>
+    Array.from(allFeatureNames).map((featureName) => (
+      <tr key={featureName}>
+        <td>{featureMapForRelatedProduct.get(featureName) || '-'}</td>
+        <td className="fw-medium">{featureName}</td>
+        <td>{getFeatureValue(selectedFeatures, featureName)}</td>
+      </tr>
+    ));
+
+  return (
+    <table className="table text-center">
+      <thead>
+        <tr className="fw-semibold">
+          <th scope="col" className="column-style">
+            {relatedProductName}
+          </th>
+          <th scope="col" className="column-style">
+            Feature
+          </th>
+          <th scope="col" className="column-style">
+            {selectedProductName}
+          </th>
+        </tr>
+      </thead>
+      <tbody>{renderFeatureRows()}</tbody>
+    </table>
+  );
+};
+
+FeatureTable.propTypes = {
+  relatedProduct: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    features: PropTypes.array.isRequired,
+  }).isRequired,
+  selectedProduct: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    features: PropTypes.array.isRequired,
+  }).isRequired,
+};
+
+export default FeatureTable;
