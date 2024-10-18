46b14c61bee8e2102773ef64dc463cc7650e4a5b
Daniel Jankowski <djank0113@gmail.com>
2024-10-18 16:31:01 -0400

    Improved the search bar by adding state management for query input and a clear button with focus functionality. Updated the icon component to support size and style props. Adjusted button styles in QuestionsList for better alignment. Added utility classes in SCSS for revealing element structure and refined search input styles.

--- client/src/components/icons.jsx
+++ client/src/components/icons.jsx
@@ -5,9 +5,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { PropTypes } from 'prop-types';
 
 library.add(...all);
-export default function Icon({ icon }) {
+export default function Icon({ icon, size, style }) {
   return icon ? (
-    <FontAwesomeIcon icon={'fa-sharp ' + icon} />
+    <FontAwesomeIcon icon={'fa-sharp ' + icon} size={size} style={style} />
   ) : (
     <div className="text-danger">missing prop value for icon</div>
   );
