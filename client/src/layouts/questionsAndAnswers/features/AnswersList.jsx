4be822ff8b79bc92e190c77b090d49c74ddc6e46
itsWalkerRichard <walker@teamrichard.com>
2024-10-18 14:29:48 -0600

    fixed show more logic for answers

--- client/src/layouts/questionsAndAnswers/features/AnswersList.jsx
+++ client/src/layouts/questionsAndAnswers/features/AnswersList.jsx
@@ -7,26 +7,20 @@ import Answer from './Answer.jsx';
 
 export default function AnswersList({ answers, question_id }) {
   const [displayedAnswers, setDisplayedAnswers] = useState(2);
-  const [moreIsHidden, setMoreIsHidden] = useState(true);
+  const [moreIsHidden, setMoreIsHidden] = useState(false);
   const [cantLoadMore, setCantLoadMore] = useState(false);
   const [maxLoadedAnswers, setMaxLoadedAnswers] = useState(
     Object.keys(answers).length,
   );
 
   useEffect(() => {
-    if (displayedAnswers > 2) {
-      setMoreIsHidden(false);
-    } else {
-      setMoreIsHidden(true);
-    }
+    displayedAnswers > 2 ? setMoreIsHidden(false) : setMoreIsHidden(true);
   }, [displayedAnswers]);
 
   useEffect(() => {
-    if (displayedAnswers >= maxLoadedAnswers) {
-      setCantLoadMore(true);
-    } else {
-      setCantLoadMore(false);
-    }
+    displayedAnswers >= maxLoadedAnswers
+      ? setCantLoadMore(true)
+      : setCantLoadMore(false);
   }, [displayedAnswers, maxLoadedAnswers]);
 
   const handleLoadMoreAnswers = () => {
@@ -45,20 +39,22 @@ export default function AnswersList({ answers, question_id }) {
           let answer = answers[key];
           return <Answer key={answer.id} answer={answer} />;
         })}
-      <button
-        className="d-inline-flex text-secondary text-size-90 bg-transparent hstack border-0 shadow-none text-decoration-underline ps-1"
-        onClick={handleLoadMoreAnswers}
-        hidden={cantLoadMore}
-      >
-        Load More Answers
-      </button>
-      <button
-        className="d-inline-flex text-secondary text-size-90 bg-transparent hstack border-0 shadow-none text-decoration-underline ps-1"
-        onClick={hideMoreAnswers}
-        hidden={moreIsHidden}
-      >
-        Hide More Answers
-      </button>
+      {!cantLoadMore && (
+        <button
+          className="d-inline-flex text-secondary text-size-90 bg-transparent hstack border-0 shadow-none text-decoration-underline ps-1"
+          onClick={handleLoadMoreAnswers}
+        >
+          Load More Answers
+        </button>
+      )}
+      {!moreIsHidden && (
+        <button
+          className="d-inline-flex text-secondary text-size-90 bg-transparent hstack border-0 shadow-none text-decoration-underline ps-1"
+          onClick={hideMoreAnswers}
+        >
+          Hide More Answers
+        </button>
+      )}
     </div>
   );
 }
