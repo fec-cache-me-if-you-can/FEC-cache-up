e2c89c46c3c8998491ecba886829134a7865632a
itsWalkerRichard <walker@teamrichard.com>
2024-10-21 13:36:07 -0600

    added pagination functionality

--- client/src/layouts/questionsAndAnswers/features/QuestionsList.jsx
+++ client/src/layouts/questionsAndAnswers/features/QuestionsList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 
 import SearchQuestions from './SearchQuestions.jsx';
@@ -13,15 +13,15 @@ export default function QuestionsList({ productId }) {
   const [displayedQuestions, setDisplayedQuestions] = useState(4);
   const [moreIsHidden, setMoreIsHidden] = useState(false);
   const [hideButton, setHideButton] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [hideNextPage, setHideNextPage] = useState(false);
+  const [hidePreviousPage, setHidePreviousPage] = useState(true);
 
   useEffect(() => {
-    console.log('==================================');
-    console.log('displayedQuestions', displayedQuestions);
-    console.log('moreIsHidden', moreIsHidden);
-    console.log('hideButton', hideButton);
-    console.log('questions.length', questions.length);
-    console.log('==================================');
-  }, [displayedQuestions, moreIsHidden, hideButton, questions]);
+    getQuestions(productId)
+      .then((result) => setQuestions(result.data.results))
+      .catch((err) => console.log(err));
+  }, [productId]);
 
   useEffect(() => {
     displayedQuestions > 4 ? setMoreIsHidden(false) : setMoreIsHidden(true);
@@ -35,25 +35,51 @@ export default function QuestionsList({ productId }) {
     }
   }, [displayedQuestions, questions.length]);
 
-  const handleLoadMoreQuestions = (e) => {
-    setDisplayedQuestions((displayedQuestions) => displayedQuestions + 2);
+  useEffect(() => {
+    getQuestions(productId, currentPage + 1)
+      .then((result) => {
+        result.data.results.length
+          ? setHideNextPage(false)
+          : setHideNextPage(true);
+      })
+      .catch((err) => console.log(err));
+  }, [currentPage, productId]);
+
+  useEffect(() => {
+    currentPage > 1 ? setHidePreviousPage(false) : setHidePreviousPage(true);
+  }, [currentPage]);
+
+  useEffect(() => {
+    getQuestions(productId, currentPage)
+      .then((result) => setQuestions(result.data.results))
+      .catch((err) => console.log(err));
+  }, [currentPage]);
+
+  const handleLoadMoreQuestions = () => {
+    setDisplayedQuestions(10);
   };
+
+  const getQuestions = (product_id, page = 1, count = 10) => {
+    return axios.get(
+      `/qa/questions?product_id=${product_id}&page=${page}&count=${count}`,
+    );
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage((page) => page - 1);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((page) => page + 1);
+  };
+
   const resetDisplayedQuestions = () => {
+    setCurrentPage(1);
     setDisplayedQuestions(4);
   };
 
-  useEffect(() => {
-    axios
-      .get(`/qa/questions?product_id=${productId}&count=20`)
-      .then((result) => {
-        setQuestions(result.data.results);
-      })
-      .catch((err) => console.log(err));
-  }, [productId]);
-
   const createQuestion = (body) => {
     body.product_id = productId;
-    console.log(body);
     return axios.post('/qa/questions', body);
   };
 
@@ -73,7 +99,32 @@ export default function QuestionsList({ productId }) {
         return <Question key={question.question_id} question={question} />;
       })}
       {!moreIsHidden && (
-        <button onClick={resetDisplayedQuestions}>Hide More Questions</button>
+        <div className="d-inline-flex">
+          {!hidePreviousPage && (
+            <button
+              hidden={hidePreviousPage}
+              className="d-inline-flex d-inline-flex text-secondary text-size-90 bg-transparent hstack border-0 shadow-none text-decoration-underline ps-1"
+              onClick={handlePreviousPage}
+            >
+              previous page
+            </button>
+          )}
+          {!hideNextPage && (
+            <button
+              hidden={hideNextPage}
+              className="d-inline-flex d-inline-flex text-secondary text-size-90 bg-transparent hstack border-0 shadow-none text-decoration-underline ps-1"
+              onClick={handleNextPage}
+            >
+              next page
+            </button>
+          )}
+          <button
+            className="d-inline-flex d-inline-flex text-secondary text-size-90 bg-transparent hstack border-0 shadow-none text-decoration-underline ps-1"
+            onClick={resetDisplayedQuestions}
+          >
+            Hide More Questions
+          </button>
+        </div>
       )}
     </div>
   );
